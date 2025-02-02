320a3d5e3bba7f997d9df44014b04a8d1a709280
jake <jakelif@umich.edu>
2025-02-01 21:35:04 -0500

    New commit

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,7 +4,6 @@ from rest_framework.generics import ListCreateAPIView, CreateAPIView, ListAPIVie
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from django.db.models import Q
 from .models import *
 from .serializers import *
 
