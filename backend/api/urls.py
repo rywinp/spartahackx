ae58907946f0e565b419b7fab54b6cf839ae05a4
jake <jakelif@umich.edu>
2025-02-01 21:02:01 -0500

    Adding more endpoints to API

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('quests/', get_quests),
-    path('')
+    path('create-parent/', create_parent, name='create-parent'),
+    path('create-child/', create_child, name='create-child'),
+    path('create-quest/', create_quest, name='create-quest'),
+    path('create-reward/', create_reward, name='create-reward'),
+    path('get-quests/', get_quests, name='get-quests'),
+    path('get-rewards/', get_rewards, name='get-rewards'),
 ]
